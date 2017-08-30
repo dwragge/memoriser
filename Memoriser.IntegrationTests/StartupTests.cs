ee44a2827f22db772b3b0a2b443438980f2042e2
Dylan Wragge <dylanwragge@gmail.com>
2017-08-30 16:46:08 +1200

    Add integration test for server startup

---
+++ Memoriser.IntegrationTests/StartupTests.cs
@@ -0,0 +1,24 @@
+﻿using Memoriser.App;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace Memoriser.IntegrationTests
+{
+    public class StartupTests
+    {
+
+        public async Task Can_Startup()
+        {
+            var server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>()
+                .ConfigureAppConfiguration(builder =>
+                    builder.AddJsonFile("appsettings.json")));
+            var httpClient = server.CreateClient();
+
+            var response = await server.CreateRequest("/").SendAsync("GET");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
