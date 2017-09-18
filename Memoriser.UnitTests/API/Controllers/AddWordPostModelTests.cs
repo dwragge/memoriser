9bd0a93dd207d203690d92b6c8de3a3e5518217f
Dylan Wragge <dylanwragge@gmail.com>
2017-09-19 10:19:08 +1200

    Consicer test

--- Memoriser.UnitTests/API/Controllers/AddWordPostModelTests.cs
+++ Memoriser.UnitTests/API/Controllers/AddWordPostModelTests.cs
@@ -1,48 +1,40 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Memoriser.App.Controllers.PostModels;
 using Xunit;
 
 namespace Memoriser.UnitTests.API.Controllers
 {
-    public class AddWordPostModelTests
+    public class AddWordPostModelTests : ValidatorTests
     {
-        public static IEnumerable<object[]> InvalidModels
-        {
-            get
-            {
-                yield return new object[] {new AddWordPostModel()};
-                yield return new object[] {new AddWordPostModel {Answers = null, Word = "Hello"}};
-                yield return new object[] {new AddWordPostModel {Answers = new[] {"Hello", "Goodbye"}, Word = null}};
-                yield return new object[] {new AddWordPostModel {Answers = new string[] { }, Word = "Hello"}};
-                yield return new object[] {new AddWordPostModel {Answers = new[] {"Hello"}, Word = "123"}};
-                yield return new object[] {new AddWordPostModel {Answers = new[] {"123"}, Word = "Hello"}};
-                yield return new object[] {new AddWordPostModel {Answers = new[] {"abc", "123 &&^"}, Word = "Hello"}};
-                yield return new object[] {new AddWordPostModel {Answers = new[] {"Hello"}, Word = " "}};
-            }
-        }
-
         [Theory]
-        [InlineData(new [] {" "}, "Hello")]
-        [InlineData(new[] { "Hello", "Goodbye" }, null)]
-        [InlineData(new string[] { }, "Hello")]
-        [InlineData(new[] { "Hello" }, "123")]
-        [InlineData(new[] { "123" }, "Hello")]
-        [InlineData(new[] { "abc", "123 &&^" }, "Hello")]
-        [InlineData(new[] { "Hello" }, " ")]
+        [InlineData(new [] {" "}, " ")]
+        [InlineData(new string[] { }, "123")]
+        [InlineData(new[] { "123" }, "123 &&^")]
         public void Validation_Should_FailForInvalid(string[] answers, string word)
         {
-            var model = new AddWordPostModel()
+            var model = new AddWordPostModel
             {
                 Answers = answers,
                 Word = word
             };
 
-            var results = new List<ValidationResult>();
-            var context = new ValidationContext(model, null, null);
-            Validator.TryValidateObject(model, context, results, true);
-            results.Count.Should().BeGreaterThan(0, $"Answers: \"{string.Join(" ,", answers)}\" and Word: \"{word}\" should fail the validation.");
+            ValidateObject(model).Count.Should().Be(2, 
+                $"Answers: \"{string.Join(" ,", answers)}\" and Word: \"{word}\" should fail both validations.");
+        }
+
+        [Theory]
+        [InlineData(new[] {"hello", "goodbye"}, "salut")]
+        [InlineData(new[] {"éclarter", "rôder"}, "word")]
+        public void Validation_Should_PassForValid(string[] answers, string word)
+        {
+            var model = new AddWordPostModel
+            {
+                Answers = answers,
+                Word = word
+            };
+
+            ValidateObject(model).Count.Should().Be(0,
+                $"Answers: \"{string.Join(" ,", answers)}\" and Word: \"{word}\" should pass the validation.");
         }
     }
 }
---
+++ Memoriser.UnitTests/API/ValidatorTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Memoriser.UnitTests.API
+{
+    public class ValidatorTests
+    {
+        protected List<ValidationResult> ValidateObject(object obj)
+        {
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(obj, null, null);
+            Validator.TryValidateObject(obj, context, results, true);
+            return results;
+        }
+    }
+}
